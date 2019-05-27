36b28ed61b97f88a03c6cd90983d7ee18fa1fc77
CarlW <carl@oskarroger.net>
2019-05-27 16:16:02 +0800

    imported react icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+
+export default class Navbar extends Component {
+  render() {
+    return <div>Hello from Navbar</div>;
+  }
+}
