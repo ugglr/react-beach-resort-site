5ed5d01df429c3ad607d0a971ec462c28f757eb3
CarlW <carl@oskarroger.net>
2019-05-28 10:05:34 +0800

    Hero components are complete

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Rooms() {
-  return <div>Hello from rooms page</div>;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
